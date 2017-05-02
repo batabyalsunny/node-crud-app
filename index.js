dabaee307b12a597aaab1312715902e4152125f8
Sunny Batabyal <batabyalsunny@gmail.com>
2017-05-02 20:33:08 +0530

    body-parser added and nav color changed.

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // set default view engine to ejs
 app.set('view engine', 'ejs');
 
-// handle get request for the landing page
+var data = [];
+
+// handle get request for the default landing page
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {data: data});
+});
+
+// handle post request
+app.post('/insert', urlencodedParser, function(req, res) {
+  data.push(req.body);
+  res.send(data);
 });
 
 // set listen port
